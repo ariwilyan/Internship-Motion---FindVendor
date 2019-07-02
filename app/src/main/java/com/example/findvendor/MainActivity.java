26540df5dc0c50de189e8577ec61a0e1c29f7c90
Muhammad Rafii Danendra <rafiidanendra8787@gmail.com>
2019-07-02 15:14:20 +0700

    Button Selanjutnya Button Daftar

--- app/src/main/java/com/example/findvendor/MainActivity.java
+++ app/src/main/java/com/example/findvendor/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.findvendor;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSelanjutnya;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSelanjutnya = findViewById(R.id.btn_selanjutnya);
+
+        btnSelanjutnya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, login.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/findvendor/login.java
+++ app/src/main/java/com/example/findvendor/login.java
@@ -1,13 +1,28 @@
 package com.example.findvendor;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
+    private Button btn_daftar_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_daftar_login = findViewById(R.id.btn_daftar_login);
+
+        btn_daftar_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, daftar.class));
+            }
+        });
     }
 }
