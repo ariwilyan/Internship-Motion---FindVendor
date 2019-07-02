00720ea0f91c17d1bc4e2f1bb444d9331d49c7d3
Muhammad Rafii Danendra <rafiidanendra8787@gmail.com>
2019-07-02 22:30:53 +0700

    Bottom Navigation page utama

--- app/src/main/java/com/example/findvendor/login.java
+++ app/src/main/java/com/example/findvendor/login.java
@@ -1,7 +1,6 @@
 package com.example.findvendor;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +9,7 @@ import android.widget.Button;
 public class login extends AppCompatActivity {
 
     private Button btn_daftar_login;
+    private Button btn_masuk_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,19 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btn_daftar_login = findViewById(R.id.btn_daftar_login);
+        btn_masuk_login = findViewById(R.id.btn_masuk_login);
 
         btn_daftar_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(login.this, daftar.class));
+            }
+        });
+
+        btn_masuk_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, utama.class));
             }
         });
     }
